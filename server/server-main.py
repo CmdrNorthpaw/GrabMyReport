a9644a96dd39e649956e449036b6dbb6b289f34e
CmdrNorthpaw <thetuxkid@gmail.com>
2020-01-13 16:47:14 +0000

    Added a socket server. It ain't done yet.

---
+++ server/server-main.py
@@ -0,0 +1,11 @@
+import discord
+import socket
+from os import environ
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind(socket.gethostname(), 9254)
+sock.listen(5)
+
+while True:
+    clientSock, address = sock.accept()
